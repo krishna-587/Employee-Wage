c6754ad2220c8589008dff6ebff22f9a1c81d4b6
Krishna gopal singh <krishnagopalsingh587@gmail.com>
2025-03-20 22:36:11 +0530

    [Krishna Gopal Singh] Add. Refactored code

---
+++ App.js
@@ -0,0 +1,33 @@
+//UC 1
+const IS_Absent = 0;
+
+// let empCheck = Math.floor(Math.random()* 10) % 2;
+
+if(empCheck == IS_Absent){
+    console.log("Employee is Absent");
+    return;
+}
+else{
+    console.log("Employee is Present");
+}
+
+
+//UC3
+
+empCheck = Math.floor(Math.random()* 10) % 3;
+
+function getWorkingHours (empCheck) {
+    switch (empCheck) {
+        case IS_PART_TIME:
+            return PART_TIME_HOURS;
+        case IS_FULL_TIME:
+            return FULL_TIME_HOURS;
+        default:
+            return 0;
+    }
+}
+    let empHrs = 0;
+    let empCheck = Math.floor(Math.random() * 10) % 3;
+    empHrs = getWorkingHours(empCheck);
+    let empwage = empHrs * WAGE_PER_HOUR;
+    console.log("Emp Wage: " + empWage);
